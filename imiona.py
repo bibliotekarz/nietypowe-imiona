3f88d2382c0e0dd098ceaa71d7f7e8cd73aa4e03
bibliotekarz <arek@bibliotekarz.com>
2021-04-05 10:41:34 +0200

    upgrade to version with data source selection

--- imiona.py
+++ imiona.py
@@ -5,10 +5,10 @@ name=[]
 def get_from_pesel(sex):
     if sex == 'K':
         response_source = 'https://api.dane.gov.pl/media/resources/20210204/Wykaz_imion_%C5%BCe%C5%84skich_os%C3%B3b_%C5%BCyj%C4%85cych_wg_pola_imi%C4%99_pierwsze_wyst%C4%99puj%C4%85cych_w_rejestrze_PESEL_bez_zgon%C3%B3w.csv'
-        message = 'Żeńskich imion nie kończących się na a jest '
+        message = 'Według bazy PESEL żeńskich imion nie kończących się na a jest '
     else:
         response_source = 'https://api.dane.gov.pl/media/resources/20210204/Wykaz_imion_m%C4%99skich_os%C3%B3b_%C5%BCyj%C4%85cych_wg_pola_imi%C4%99_pierwsze_wyst%C4%99puj%C4%85cych_w_rejestrze_PESEL_bez_zgon%C3%B3w.csv'
-        message = 'Męskich imion kończących się na a jest '
+        message = 'Według bazy PESEL męskich imion kończących się na a jest '
 
     with urllib.request.urlopen(response_source) as response:
         lines1 = response.read()
@@ -26,30 +26,51 @@ def get_from_pesel(sex):
     print(message + str(len(name)))
 
 
+
+def get_from_wiki(sex):
+
+    if sex == 'K':
+        file_source = './src/imiona_damskie.txt'
+        message = 'Według Wikipedii żeńskich imion nie kończących się na a jest '
+    else:
+        file_source = './src/imiona_meskie.txt'
+        message = 'Według Wikipedii męskich imion kończących się na a jest '
+
+    with open(file_source, "r", encoding='utf8') as f:
+        lines = f.read()
+        rows = lines.split('\n')
+        for row in rows:
+            if sex == 'K':
+                if len(row) > 1 and row[-1] !='a':
+                    name.append(row)
+            else:
+                if len(row) > 1 and row[-1] =='a':
+                    name.append(row)
+    
+    print('')
+    print(name)
+    print(message + str(len(name)))
+    print('')
+
+
 source =''
 sex=''
 data_complete = False
 while data_complete != True:
-    
-    print(f'zastane source to{source}_')
     if source != 'W' and source != 'P':
         source = input('Wpisz P żeby obrobić dane z bazy (P)ESEL, albo wpisz W żeby przetworzyć listę imion z (W)ikipedii.\n :')
         source = source.upper()
-        print('Literówka jakaś?\n')
-        print(f'wprowadzone source to {source}')
         continue
     else:
-        print('Idziemy dalej')
         if sex != "K" and sex != "M":
-            sex = input('Wpisz K żeby wybrać imiona (K)obiece, albo wpisz M żeby wybrać imiona (M)ęskie.\n :')
+            sex = input('Teraz wpisz K żeby wybrać imiona (K)obiece, albo wpisz M żeby wybrać imiona (M)ęskie.\n :')
             sex = sex.upper()
-            print('Literówka jakaś?\n')
-            print(f'wprowadzona płeć to {sex}')
+            print('')
             continue
         else:
             data_complete = True
-            print(f'Wybrałeś źródło {source} i płeć {sex}')
             if source == 'P':
                 get_from_pesel(sex)
             else:
                 get_from_wiki(sex)
+
